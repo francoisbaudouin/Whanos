fa1fab01d08fca2372d9821c6753bebffb60c7f6
François <francois.baudouin@epitech.eu>
2023-12-12 09:44:21 +0100

    add terraform requirement

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.14.0" # Takes into account Terraform versions from 0.14.0
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.42.0"
+    }
+
+    ovh = {
+      source  = "ovh/ovh"
+      version = ">= 0.13.0"
+    }
+  }
+}
+
 provider "ovh" {
   endpoint    = "ovh-eu"  # Replace with the appropriate OVH endpoint for your region
   application_key    = "your_application_key"
