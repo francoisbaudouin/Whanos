1a2b82570f9e8ef790e5682dceef45ca14d85d93
François <francois.baudouin@epitech.eu>
2023-12-12 09:42:00 +0100

    add terraform deployement

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "ovh" {
+  endpoint    = "ovh-eu"  # Replace with the appropriate OVH endpoint for your region
+  application_key    = "your_application_key"
+  application_secret = "your_application_secret"
+  consumer_key       = "your_consumer_key"
+}
